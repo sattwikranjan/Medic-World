d555325d8337c52a3928501e7721e51738f308b7
Sattwik Ranjan <sattwikranjan@gmail.com>
2023-06-25 20:46:20 +0530

    User Login Backend created

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,13 +1,27 @@
 import React from "react";
-import { Form, Input } from "antd";
+import { Form, Input, message } from "antd";
+import axios from "axios";
 import "../styles/RegisterStyles.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
-//form handler
-const onfinishHandler = (values) => {
-  console.log(values);
-};
 const Login = () => {
+  const navigate = useNavigate();
+  //form handler
+  const onfinishHandler = async (values) => {
+    try {
+      const res = await axios.post("/api/v1/user/login", values);
+      if (res.data.success) {
+        localStorage.setItem("token", res.data.token);
+        message.success("Login Successfully");
+        navigate("/");
+      } else {
+        message.error(res.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      message.error("Something went wrong");
+    }
+  };
   return (
     <div className="form-container">
       <Form
