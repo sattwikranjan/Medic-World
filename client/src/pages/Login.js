e6761adf88fbf9f08b04c3cce8821dac9198106a
Sattwik Ranjan <sattwikranjan@gmail.com>
2023-06-25 12:06:19 +0530

    Login Page Created and npm package concurrently installed

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,9 +1,34 @@
 import React from "react";
+import { Form, Input } from "antd";
+import "../styles/RegisterStyles.css";
+import { Link } from "react-router-dom";
 
+//form handler
+const onfinishHandler = (values) => {
+  console.log(values);
+};
 const Login = () => {
   return (
-    <div>
-      <h1>Login Page</h1>
+    <div className="form-container">
+      <Form
+        layout="vertical"
+        onFinish={onfinishHandler}
+        className="register-form"
+      >
+        <h1 className="text-center">Login Form</h1>
+        <Form.Item label="Email" name="email">
+          <Input type="email" required />
+        </Form.Item>
+        <Form.Item label="Password" name="password">
+          <Input type="password" required />
+        </Form.Item>
+        <Link to="/register" className="m-2">
+          Already a user Register here
+        </Link>
+        <button className="btn btn-primary" type="submit">
+          Login
+        </button>
+      </Form>
     </div>
   );
 };
