031ab2bab94d398f57dbeceaee00f83d5cb9b448
Sattwik Ranjan <sattwikranjan@gmail.com>
2023-06-25 11:28:48 +0530

    Register Page Created and CSS added

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,11 +1,41 @@
-import React from 'react'
+import React from "react";
+import "../styles/RegisterStyles.css";
+import { Form, Input } from "antd";
+import { Link } from "react-router-dom";
 
 const Register = () => {
+  //form handler
+  const onfinishHandler = (values) => {
+    console.log(values);
+  };
   return (
-    <div>
-        <h1>Register Page</h1>
-    </div>
-  )
-}
+    <>
+      <div className="form-container">
+        <Form
+          layout="vertical"
+          onFinish={onfinishHandler}
+          className="register-form"
+        >
+          <h1 className="text-center">Register Form</h1>
+          <Form.Item label="Name" name="name">
+            <Input type="text" required />
+          </Form.Item>
+          <Form.Item label="Email" name="email">
+            <Input type="email" required />
+          </Form.Item>
+          <Form.Item label="Password" name="password">
+            <Input type="password" required />
+          </Form.Item>
+          <Link to="/login" className="m-2">
+            Already user Login here
+          </Link>
+          <button className="btn btn-primary" type="submit">
+            Register
+          </button>
+        </Form>
+      </div>
+    </>
+  );
+};
 
-export default Register
+export default Register;
