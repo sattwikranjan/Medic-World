c3d11f738ddd83dbce0ead52a8d0b65fc0d112d4
Sattwik Ranjan <sattwikranjan@gmail.com>
2023-07-15 09:42:30 +0530

    Minor changes

--- client/src/pages/doctor/Profile.js
+++ client/src/pages/doctor/Profile.js
@@ -75,128 +75,126 @@ const Profile = () => {
     //eslint-disable-next-line
   }, []);
   return (
-    <div>
-      <Layout>
-        <h1>Manage Profile</h1>
-        {doctor && (
-          <Form
-            layout="vertical"
-            onFinish={handleFinish}
-            className="m-3"
-            initialValues={{
-              ...doctor,
-              timings: [
-                moment(doctor.timings[0], "HH:mm"),
-                moment(doctor.timings[1], "HH:mm"),
-              ],
-            }}
-          >
-            <h4 className="">Personal Details : </h4>
-            <Row gutter={20}>
-              <Col xs={24} md={24} lg={8}>
-                <Form.Item
-                  label="First Name"
-                  name="firstName"
-                  required
-                  rules={[{ required: true }]}
-                >
-                  <Input type="text" placeholder="your first name" />
-                </Form.Item>
-              </Col>
-              <Col xs={24} md={24} lg={8}>
-                <Form.Item
-                  label="Last Name"
-                  name="lastName"
-                  required
-                  rules={[{ required: true }]}
-                >
-                  <Input type="text" placeholder="your last name" />
-                </Form.Item>
-              </Col>
-              <Col xs={24} md={24} lg={8}>
-                <Form.Item
-                  label="Phone No"
-                  name="phone"
-                  required
-                  rules={[{ required: true }]}
-                >
-                  <Input type="text" placeholder="your contact no" />
-                </Form.Item>
-              </Col>
-              <Col xs={24} md={24} lg={8}>
-                <Form.Item
-                  label="Email"
-                  name="email"
-                  required
-                  rules={[{ required: true }]}
-                >
-                  <Input type="email" placeholder="your email address" />
-                </Form.Item>
-              </Col>
-              <Col xs={24} md={24} lg={8}>
-                <Form.Item label="Website" name="website">
-                  <Input type="text" placeholder="your website" />
-                </Form.Item>
-              </Col>
-              <Col xs={24} md={24} lg={8}>
-                <Form.Item
-                  label="Address"
-                  name="address"
-                  required
-                  rules={[{ required: true }]}
-                >
-                  <Input type="text" placeholder="your clinic address" />
-                </Form.Item>
-              </Col>
-            </Row>
-            <h4>Professional Details :</h4>
-            <Row gutter={20}>
-              <Col xs={24} md={24} lg={8}>
-                <Form.Item
-                  label="Specialization"
-                  name="specialization"
-                  required
-                  rules={[{ required: true }]}
-                >
-                  <Input type="text" placeholder="your specialization" />
-                </Form.Item>
-              </Col>
-              <Col xs={24} md={24} lg={8}>
-                <Form.Item
-                  label="Experience"
-                  name="experience"
-                  required
-                  rules={[{ required: true }]}
-                >
-                  <Input type="text" placeholder="your experience" />
-                </Form.Item>
-              </Col>
-              <Col xs={24} md={24} lg={8}>
-                <Form.Item
-                  label="Fees Per Cunsaltation"
-                  name="feesperCunsaltation"
-                  required
-                  rules={[{ required: true }]}
-                >
-                  <Input type="text" placeholder="your contact no" />
-                </Form.Item>
-              </Col>
-              <Col xs={24} md={24} lg={8}>
-                <Form.Item label="Timings" name="timings" required>
-                  <TimePicker.RangePicker format="HH:mm" />
-                </Form.Item>
-              </Col>
-              <Col xs={24} md={24} lg={8}></Col>
-              <Col xs={24} md={24} lg={8}>
-                <button className="btn btn-primary form-btn" type="submit">
-                  Update
-                </button>
-              </Col>
-            </Row>
-          </Form>
-        )}
-      </Layout>
-    </div>
+    <Layout>
+      <h1>Manage Profile</h1>
+      {doctor && (
+        <Form
+          layout="vertical"
+          onFinish={handleFinish}
+          className="m-3"
+          initialValues={{
+            ...doctor,
+            timings: [
+              moment(doctor.timings[0], "HH:mm"),
+              moment(doctor.timings[1], "HH:mm"),
+            ],
+          }}
+        >
+          <h4 className="">Personal Details : </h4>
+          <Row gutter={20}>
+            <Col xs={24} md={24} lg={8}>
+              <Form.Item
+                label="First Name"
+                name="firstName"
+                required
+                rules={[{ required: true }]}
+              >
+                <Input type="text" placeholder="your first name" />
+              </Form.Item>
+            </Col>
+            <Col xs={24} md={24} lg={8}>
+              <Form.Item
+                label="Last Name"
+                name="lastName"
+                required
+                rules={[{ required: true }]}
+              >
+                <Input type="text" placeholder="your last name" />
+              </Form.Item>
+            </Col>
+            <Col xs={24} md={24} lg={8}>
+              <Form.Item
+                label="Phone No"
+                name="phone"
+                required
+                rules={[{ required: true }]}
+              >
+                <Input type="text" placeholder="your contact no" />
+              </Form.Item>
+            </Col>
+            <Col xs={24} md={24} lg={8}>
+              <Form.Item
+                label="Email"
+                name="email"
+                required
+                rules={[{ required: true }]}
+              >
+                <Input type="email" placeholder="your email address" />
+              </Form.Item>
+            </Col>
+            <Col xs={24} md={24} lg={8}>
+              <Form.Item label="Website" name="website">
+                <Input type="text" placeholder="your website" />
+              </Form.Item>
+            </Col>
+            <Col xs={24} md={24} lg={8}>
+              <Form.Item
+                label="Address"
+                name="address"
+                required
+                rules={[{ required: true }]}
+              >
+                <Input type="text" placeholder="your clinic address" />
+              </Form.Item>
+            </Col>
+          </Row>
+          <h4>Professional Details :</h4>
+          <Row gutter={20}>
+            <Col xs={24} md={24} lg={8}>
+              <Form.Item
+                label="Specialization"
+                name="specialization"
+                required
+                rules={[{ required: true }]}
+              >
+                <Input type="text" placeholder="your specialization" />
+              </Form.Item>
+            </Col>
+            <Col xs={24} md={24} lg={8}>
+              <Form.Item
+                label="Experience"
+                name="experience"
+                required
+                rules={[{ required: true }]}
+              >
+                <Input type="text" placeholder="your experience" />
+              </Form.Item>
+            </Col>
+            <Col xs={24} md={24} lg={8}>
+              <Form.Item
+                label="Fees Per Cunsaltation"
+                name="feesperCunsaltation"
+                required
+                rules={[{ required: true }]}
+              >
+                <Input type="text" placeholder="your contact no" />
+              </Form.Item>
+            </Col>
+            <Col xs={24} md={24} lg={8}>
+              <Form.Item label="Timings" name="timings" required>
+                <TimePicker.RangePicker format="HH:mm" />
+              </Form.Item>
+            </Col>
+            <Col xs={24} md={24} lg={8}></Col>
+            <Col xs={24} md={24} lg={8}>
+              <button className="btn btn-primary form-btn" type="submit">
+                Update
+              </button>
+            </Col>
+          </Row>
+        </Form>
+      )}
+    </Layout>
   );
 };
 
